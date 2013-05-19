455b653bfa5d2027a7d8f77f80f88616eaaf57c7
djey <j.canler@gmail.com>
2013-05-19 15:11:28 +0200

    Added Redis cache handler and collector classes (empty for now)

---
+++ GrayBox/rupees/cache/rediscache.rb
@@ -0,0 +1,8 @@
+#rediscache.rb
+#To address cache, handled by Redis database
+class RedisCache
+  def store
+    #TODO input parameters ??
+    #Should call cache
+  end
+end
---
+++ GrayBox/rupees/collector/collector.rb
@@ -0,0 +1,8 @@
+#collector.rb
+#Provides methods to handle information storage
+class Collector
+  
+  def add(appId, contextId, natureId, value)
+    #Should call cache here
+  end
+end
