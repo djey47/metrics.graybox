3aeb5429cb677b7ce93d5e852b80af12694d75aa
unknown <djey@.(none)>
2013-05-21 22:52:09 +0200

    Added start scripts for Windows and Linux

---
+++ GrayBox/scripts/start-metrics.cmd
@@ -0,0 +1,5 @@
+@ECHO OFF
+REM Starts Metrics Controller for Windows
+REM TODO Should check for prerequisites (ruby)
+ruby ..\rupees\controller\controller.rb
+PAUSE
---
+++ GrayBox/scripts/start-metrics.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+#Starts Metrics Controller for Linux
+#TODO Should check for prerequisites (ruby)
+ruby ../rupees/controller/controller.rb
