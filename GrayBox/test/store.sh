9e145581700af3910e10037ac54a5e020554088d
unknown <djey@.(none)>
2013-07-16 22:55:48 +0200

    Fixed script for servers not accepting requests without a body

--- GrayBox/test/store.sh
+++ GrayBox/test/store.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 #Test basic store functionality
-curl -X POST http://localhost:4567/collector/APP_TEST/CTX1/NAT1/45.00
+curl -X POST -d "" http://localhost:4567/collector/APP_TEST/CTX1/NAT1/45.00
