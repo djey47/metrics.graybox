4325a89f19dea830aa9205b903782d82def9f58c
unknown <djey@.(none)>
2013-08-06 00:06:39 +0200

    [MET-4] Added info to script

--- GrayBox/test/store_loop.sh
+++ GrayBox/test/store_loop.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 #Test basic store feature (loop over 1k items)
+echo Will publish @APP_TEST/STORE/LOOP !
 for i in {1..1000}
   	do
 		curl -X POST -d "" http://localhost:4567/collector/APP_TEST/STORE/LOOP/$i
 	done
+echo done.
