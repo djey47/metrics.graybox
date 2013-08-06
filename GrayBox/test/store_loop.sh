632e3bb3f9f1fa1a8848e225d354971da87095e9
jcanler <jcanler.ext@orange.com>
2013-08-06 18:04:12 +0200

    [MET-4] Added duration info for benchs

--- GrayBox/test/store_loop.sh
+++ GrayBox/test/store_loop.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
 #Test basic store feature (loop over 1k items)
 echo Will publish @APP_TEST/STORE/LOOP !
+
+start=`date +%s%N`
+start="$(( 10#$start ))"
 for i in {1..1000}
   	do
-		curl -X POST -d "" http://localhost:4567/collector/APP_TEST/STORE/LOOP/$i
+  		e=$(date +%s.%N)
+		curl -X POST -d "" http://localhost:4567/collector/APP_TEST/STORE/LOOP/$i-$e
 	done
-echo done.
+finish=`date +%s%N`
+finish="$(( 10#$finish ))"
+diff=$(($finish-$start))
+
+printf "=============\n"
+printf "Done in $(($diff/1000000)) ms"
+printf "\n"
+avg=$(($diff/1000/1000000))
+printf "Average: $avg ms/req <-> $((1000/$avg)) req/s (Hz)"
+printf "\n"
